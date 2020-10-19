4b2e8fd959151aed7bb559e0fdc62cb9b67d9ecb
DinhTruong <dinh.truong24695@gmail.com>
2020-10-19 11:37:30 +0700

    fix if statment & add exception

--- count/features/step_definitions/count_steps.rb
+++ count/features/step_definitions/count_steps.rb
@@ -2,20 +2,20 @@ Given('Open browser and go to tiki homepage') do
     $driver.get 'https://tiki.vn'
   end
   
-  When('I search anything such as: {string}') do |string|
-    @product_name= string
+  When('I search any products I want') do 
+    @product_name= ''
     Kernel.puts "Please enter product you want to search:" + @product_name
     @product_name= gets.chomp
     @search_bar= $driver.find_element(:css,".eUnWAD")
-    @sub = $driver.find_element(:css, ".secondary")
-    if @sub.displayed? == true
-        @sub.click
+    begin
+        if $driver.find_element(:css, ".secondary").displayed? == true
+            $driver.find_element(:css, ".secondary").click
+        end
+        
+    rescue => exception
         @search_bar.send_keys(@product_name)
         @search_btn= $driver.find_element(:css, ".ieXBRV").click
-    else
-        @search_bar.send_keys(@product_name)
-        @search_btn= $driver.find_element(:css, ".ieXBRV").click
-    end    
+    end
   end
   
   Then('I want to know how many products will be displayed in the first page') do
